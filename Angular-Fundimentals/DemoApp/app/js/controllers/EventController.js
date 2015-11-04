f1a76a1fb1bd805b729d157a53e9a77a2fdb38c1
patty.johnson <patty.johnson@mrm-mccann.com>
2015-11-04 11:05:17 -0700

    Beginning Creating and Using Angular Services

--- Angular-Fundimentals/DemoApp/app/js/controllers/EventController.js
+++ Angular-Fundimentals/DemoApp/app/js/controllers/EventController.js
@@ -4,48 +4,9 @@
 'use strict';
 
 eventsApp.controller('EventController',
-	function EventController($scope) {
-		$scope.sortorder = '-upVoteCount';
-		$scope.event = {
-			name: 'Angular Boot Camp',
-			date: 1359781015626,
-			time: '10:30 am',
-			location: {
-				address: 'Google Headquarters',
-				city: 'Mountain View',
-				province: 'CA',
-				upVoteCount: 0
-			},
-			imageUrl: '/img/angularjs-logo.png',
-			sessions: [
-				{
-					name: 'Directives Masterclass',
-					creatorName: 'Bob Smith',
-					duration: '1 hr',
-					level: 'Advanced',
-					abstract: 'In this session you will learn the ins and outs of directives!',
-					upVoteCount: 0
-				},
-				{
-					name: 'Scopes for fun and profit',
-					creatorName: 'John Doe',
-					duration: '30 mins',
-					level: 'Introductory',
-					abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to' +
-					' get them to do it for you.',
-					upVoteCount: 0
-				},
-				{
-					name: 'Well Behaved Controllers',
-					creatorName: 'Jane Doe',
-					duration: '2 hrs',
-					level: 'Intermediate',
-					abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that' +
-					' will win the respect of your friends and neighbors.',
-					upVoteCount: 0
-				}
-			]
-		}
+	function EventController($scope, eventData) {
+		$scope.sortorder = 'name';
+		$scope.event = eventData.event;
 
 		$scope.upVoteSession = function(session) {
 			session.upVoteCount++;
